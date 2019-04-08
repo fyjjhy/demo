61de25c9788e16384eea6ae0fcfa71b1f28350b1
fuyongjie <15052418028@139.com>
2019-04-08 13:31:40 +0800

    0408

---
+++ src/main/java/collection/ListDemo1.java
@@ -0,0 +1,19 @@
+package collection;
+
+/**
+ * <Description> List<br>
+ * 【链接地址】
+ * https://www.cnblogs.com/gaojing/archive/2012/06/17/java-list-sublist-caution.html
+ * @author 付永杰<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2019/4/8
+ * @see collection <br>
+ * @since V1.0<br>
+ */
+public class ListDemo1 {
+
+	public static void main(String[] args) {
+
+	}
+}
